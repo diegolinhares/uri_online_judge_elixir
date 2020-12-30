1b92d3642dff409eaeb0f39870c23ba70c513a9d
Diego Linhares <diegomlinhares@gmail.com>
2020-12-30 14:08:21 -0300

    Add problem 1040

---
+++ lib/problem_1040.ex
@@ -0,0 +1,15 @@
+defmodule UriOnlineJudge.Problem1040 do
+  def start do
+    grades = IO.gets("") |> grades_sum()
+
+    IO.puts(grades / 4)
+  end
+
+  defp grades_sum(grades) do
+      grades
+      |> String.trim_trailing()
+      |> String.split(" ")
+      |> Enum.map(&String.to_float()/1)
+      |> Enum.sum()
+  end
+end
