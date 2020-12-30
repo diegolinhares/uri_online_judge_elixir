05f3973a2879da35ce8c0ca2bfa8dcb7a3a086e3
Diego Linhares <diegomlinhares@gmail.com>
2020-12-30 12:30:26 -0300

    First commit

---
+++ lib/uri_online_judge.ex
@@ -0,0 +1,18 @@
+defmodule UriOnlineJudge do
+  @moduledoc """
+  Documentation for `UriOnlineJudge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UriOnlineJudge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uri_online_judge_test.exs
@@ -0,0 +1,8 @@
+defmodule UriOnlineJudgeTest do
+  use ExUnit.Case
+  doctest UriOnlineJudge
+
+  test "greets the world" do
+    assert UriOnlineJudge.hello() == :world
+  end
+end
